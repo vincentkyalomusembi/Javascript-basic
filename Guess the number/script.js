3b3d860330820f4f71232d7fd57961907c88e2f3
vincentkyalomusembi <https://github.com/vincentkyalomusembi/palindrome>
2025-02-03 00:03:04 +0300

    DOM manipulation

--- Guess the number/script.js
+++ Guess the number/script.js
@@ -19,21 +19,31 @@ document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
+    // when there is no input
     if(!guess){
         document.querySelector('.message').textContent = 'No number!';
+       
+        //when player wins
     } else if (guess === secretNumber){
         document.querySelector('.message').textContent = 'correct number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+
+
+        // when input is high
     }else if(guess > secretNumber){
-        if(score > 0){
+        if(score > 1){
         document.querySelector('.message').textContent =  'Too high'
         score--;
         document.querySelector('.score').textContent = score;
         }else{
             document.querySelector('.message').textContent = 'You lost the game!'
         } 
+        // when input is low
     }else if(guess < secretNumber){
         document.querySelector('.message').textContent =  'Too low'
-        score++;
+        score--;
         document.querySelector('.score').textContent = score; 
     }
 });
