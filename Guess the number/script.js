efc5b9d90eb0aa36feff1e951f38a6bf1a865ff7
vincentkyalomusembi <https://github.com/vincentkyalomusembi/palindrome>
2025-02-03 13:08:34 +0300

    modal

--- Guess the number/script.js
+++ Guess the number/script.js
@@ -13,7 +13,7 @@ document.querySelector('.guess').value = 23;
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -26,6 +26,7 @@ document.querySelector('.check').addEventListener('click', function () {
         //when player wins
     } else if (guess === secretNumber){
         document.querySelector('.message').textContent = 'correct number!';
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
 
@@ -46,4 +47,16 @@ document.querySelector('.check').addEventListener('click', function () {
         score--;
         document.querySelector('.score').textContent = score; 
     }
+});
+ 
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    document.querySelector('.message').textContent= 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
